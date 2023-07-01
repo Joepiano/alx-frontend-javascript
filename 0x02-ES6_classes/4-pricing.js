58e93832d48b80d6ea565bcae88f86490150cf87
Joepiano <99508912+Joepiano@users.noreply.github.com>
2023-06-30 17:08:17 -0700

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,36 +1,39 @@
+// Import Currency class from 3-currency.js
 import Currency from './3-currency';
 
 export default class Pricing {
-  /* eslint-disable */
   constructor(amount, currency) {
-    this._currency = currency;
+    // Create objs
     this._amount = amount;
+    this._currency = currency;
   }
 
-  // getter 
-  get currency() {
-    return this._currency;
+  // Methods
+
+  displayFullPrice() { // Display the class instance (string) with amount and currency
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
   }
 
-  get amount() {
+  static convertPrice(amount, conversionRate) { // Static method to convert price
+    return amount * conversionRate; // Convert price using conversion rate (number)
+  }
+
+  // Setters for amount and currency attributes for the class
+  set amount(newAmount) { // Setter for amount attribute (number)
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) { // Setter for currency attribute (Currency class)
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  // Getters for amount and currency attributes for the class
+  get amount() { // Getter for amount attribute (string)
     return this._amount;
   }
 
-  // setter
-  set currency(Currency) {
-    this._currency = Currency;
-  }
-
-  set amount(Amount) {
-    this._amount = Amount;
-  }
-
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`; 
-  }
-
-  // static that calculates the amount multiplied by the conversion rate
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+  get currency() { // Getter for currency attribute (Currency class)
+    return this._currency;
   }
 }
